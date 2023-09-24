b2cdc6393bad39dac807bffd8eb63e4a1279ed57
Ulf Dellbrügge <ulfdellbruegge@gmail.com>
2023-09-24 22:29:36 +0200

    toggle burger

--- app/app.js
+++ app/app.js
@@ -1,7 +1,20 @@
 async function updateClock() {
-    const res = await fetch("http://localhost:8081/clock");
-    const time = await res.text();
-    document.getElementById("clock").innerText = time;
-  }
+  const res = await fetch("http://localhost:8081/clock");
+  const time = await res.text();
+  document.getElementById("clock").innerText = time;
+}
 
-  setInterval(updateClock, 1000);
+setInterval(updateClock, 1000);
+
+burger = document.getElementById("burger");
+menu = document.getElementById("menu");
+app = document.getElementById("app");
+burger.addEventListener("click", () => {
+  if (menu.style.display === "none") {
+    menu.style.display = "flex";
+    app.style.display = "none";
+  } else {
+    menu.style.display = "none";
+    app.style.display = "flex";
+  }
+});
